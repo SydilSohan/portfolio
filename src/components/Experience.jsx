7757843ea8e6080053efbf980a539970fb11a9b2
Sydil Sohan <mdsohan383@gmail.com>
2022-10-03 23:12:48 +0600

    Skills Section Done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,136 @@
 import React from "react";
+import {HiBadgeCheck} from "react-icons/hi"
 function Experience  ()  {
     return (
-        <section className="h-screen" id="experience">
-            Experience
+        <section className="h-screen mx-auto" id="experience">
+           <h5> Skills I have</h5>
+           <h2>My Experience</h2>
+
+           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+           <div className="py-12 px-6 sm:px-32 border border-solid border-transparent rounded-2xl sm:my-12 hover:border-bg-variant grid grid-cols-2 gap-6 bg-bg-variant hover:bg-transparent transition-all ease-in-out 200ms">
+            <h2 className="col-span-2 text-white">Front End Development</h2>
+                
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                
+                
+               
+            </div>
+
+            <div className="py-12 px-6 sm:px-32 border border-solid border-transparent rounded-2xl sm:my-12 hover:border-bg-variant grid grid-cols-2 gap-6 bg-bg-variant hover:bg-transparent transition-all ease-in-out 200ms">
+            <h2 className="col-span-2 text-white">Front End Development</h2>
+                
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                <div className="flex text-left gap-1 sm:gap-2 md:gap-4">
+                    <HiBadgeCheck className="text-primary text-xl mt-1 "/>
+                        <div>
+                        <h5 className="text-left text-white">HTML</h5>
+                    <span className="text-left text-light">Experienced</span>
+                        </div>
+                   
+
+                </div>
+                
+                
+               
+            </div>
+           </div>
         </section>
     )}
     export default Experience;
